aff7d1cbc78dee411f26669cc8cb9438a8c14981
gabriel55574 <oliveira.gabrielviniciusoliver@gmail.com>
2026-01-13 20:14:55 -0300

    ajustes visuais

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -13,14 +13,14 @@ export const Layout = ({ children }: LayoutProps) => {
       <div className="hidden md:block">
         <Sidebar />
       </div>
-      
+
       {/* Main Content */}
       <main className="md:ml-64 min-h-screen pb-24 md:pb-8">
-        <div className="max-w-6xl mx-auto p-4 md:p-8">
+        <div className="w-full p-4 md:p-8">
           {children}
         </div>
       </main>
-      
+
       {/* Mobile Navigation */}
       <MobileNav />
     </div>
